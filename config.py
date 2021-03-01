a887489a053e9ac74734af098624506352a6ac2e
DragonZzzz <m15308260580@163.com>
2021-03-01 14:35:17 +0800

    0301:asl_Dataset

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ img_w = 64
 img_h = 64
 
 batch_size = 128
-learning_rate = 1e-7   # 网络学习率
+learning_rate = 3e-7   # 网络学习率
 epoches = 40 # 网络训练迭代次数
 log_interval = 2 # 打印间隔，默认每2个batch_size打印一次
 save_interval = 2 # 模型保存间隔，默认每个epoch保存一次
 num_classes = 24 # 训练分类类别数量
 model_name = 'resnet18' # 分类网络
-experiments_name = '0226_asl_lr1e-7_64'
+experiments_name = '0226_asl_3r1e-7_64'
 
